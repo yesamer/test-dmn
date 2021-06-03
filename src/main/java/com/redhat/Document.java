e055ab0d304942daf362a992ba448bc870408204
pamAdmin <>
2021-06-03 14:05:21 +0100

    {/src/main/java/com/redhat/Document.java}

--- src/main/java/com/redhat/Document.java
+++ src/main/java/com/redhat/Document.java
@@ -6,12 +6,35 @@ package com.redhat;
 
 public class Document implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Document() {
-    }
+	private java.lang.String docName;
+	private java.util.List<java.math.BigDecimal> footNoteCodes;
 
+	public Document() {
+	}
 
+	public java.lang.String getDocName() {
+		return this.docName;
+	}
 
+	public void setDocName(java.lang.String docName) {
+		this.docName = docName;
+	}
+
+	public java.util.List<java.math.BigDecimal> getFootNoteCodes() {
+		return this.footNoteCodes;
+	}
+
+	public void setFootNoteCodes(
+			java.util.List<java.math.BigDecimal> footNoteCodes) {
+		this.footNoteCodes = footNoteCodes;
+	}
+
+	public Document(java.lang.String docName,
+			java.util.List<java.math.BigDecimal> footNoteCodes) {
+		this.docName = docName;
+		this.footNoteCodes = footNoteCodes;
+	}
 
 }
